0677e5844104030ff0354b164f2c31c83abe7301
jon uy <jonxuy@gmail.com>
2015-06-09 08:32:50 -0400

    Modified template workflow - clean and copy step added to default grunt tasks - phaser js loaded from cdn - deploy/ folder git ignored

--- GruntFile.js
+++ GruntFile.js
@@ -1,38 +1,51 @@
 module.exports = function (grunt) {
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-open');
-    grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-open');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        connect: {
-            server: {
-                options: {
-                    port: 8080,
-                    base: './deploy'
-                }
-            }
-        },
-        concat: {
-            dist: {
-                src: [  "src/lib/**/*.js",
-                    "src/game/**/*.js"
-                     ],
-                dest: 'deploy/js/<%= pkg.name %>.js'
-            }
-        },
-        watch: {
-            files: 'src/**/*.js',
-            tasks: ['concat']
-        },
-        open: {
-            dev: {
-                path: 'http://localhost:8080/index.html'
-            }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: [
+      'deploy'
+    ],
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          base: './deploy'
         }
-    });
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          {src: ['src/index.html'], dest: 'deploy/index.html'},
+          {src: ['src/css/game.css'], dest: 'deploy/css/game.css'}
+        ]
+      }
+    },
+    concat: {
+      dist: {
+        src: [  "src/lib/**/*.js",
+          "src/game/**/*.js"
+           ],
+        dest: 'deploy/js/<%= pkg.name %>.js'
+      }
+    },
+    watch: {
+      files: 'src/**/*.js',
+      tasks: ['concat']
+    },
+    open: {
+      dev: {
+        path: 'http://localhost:8080/index.html'
+      }
+    }
+  });
 
-    grunt.registerTask('default', ['concat', 'connect', 'open', 'watch']);
+  grunt.registerTask('default', ['clean', 'copy', 'concat', 'connect', 'open', 'watch']);
 
 }
